cd4b0fab1e594e13aef020dddee2422732ea9d90
Joseph Kossi A <42009253+jkossi@users.noreply.github.com>
2023-06-19 19:45:20 +0000

    Adds table for phone number verifications

---
+++ app/models/phone_number_verfication.rb
@@ -0,0 +1,9 @@
+class PhoneNumberVerfication < ApplicationRecord
+  include PhoneNumberValidatable
+
+  has_one_time_password interval: 120, after_column_name: :last_otp_at
+  belongs_to :user, optional: true
+
+  validates :phone_number, presence: true, uniqueness: true
+  validates :user_id, uniqueness: { allow_nil: true }
+end

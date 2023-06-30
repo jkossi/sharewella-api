1aa4847f6178fe5f1f3f57253b10116291b3cd10
Kossi Fioklou <kossifioklou10@gmail.com>
2023-06-30 16:48:06 -0700

    Add order_item model

---
+++ app/models/order_item.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+class OrderItem < ApplicationRecord
+    belongs_to :order
+    belongs_to :package
+
+    validates :quantity, presence: true
+    validates :quantity, numericality: { greater_than_or_equal_to: 1 }
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_29_000705) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_29_204751) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,6 +23,16 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_29_000705) do
     t.bigint "user_id", null: false
     t.index ["jti"], name: "index_allowlisted_jwts_on_jti", unique: true
     t.index ["user_id"], name: "index_allowlisted_jwts_on_user_id"
+  end
+
+  create_table "order_items", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.bigint "order_id", null: false
+    t.bigint "package_id", null: false
+    t.integer "quantity"
+    t.datetime "updated_at", null: false
+    t.index ["order_id"], name: "index_order_items_on_order_id"
+    t.index ["package_id"], name: "index_order_items_on_package_id"
   end
 
   create_table "orders", force: :cascade do |t|
@@ -80,6 +90,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_29_000705) do
   end
 
   add_foreign_key "allowlisted_jwts", "users", on_delete: :cascade
+  add_foreign_key "order_items", "orders"
+  add_foreign_key "order_items", "packages"
   add_foreign_key "orders", "users"
   add_foreign_key "packages", "users", column: "creator_id"
 end

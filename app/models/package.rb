6ec8d1c86a9c2a2cc0765fc6e02bd9448b8db7b3
Onomeh Onwuanibe <60798978+Estherstarr@users.noreply.github.com>
2023-06-30 00:10:56 +0000

    Add package model

---
+++ app/models/package.rb
@@ -0,0 +1,10 @@
+class Package < ApplicationRecord
+    
+    enum status: {
+        pending: 0,
+        paid: 1
+      }
+    
+    belongs_to :creator, class_name: "User"
+    validates :name, presence: true
+end

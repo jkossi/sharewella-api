0ed5d28601eeafb317319476aba0c79da4217d5b
Kossi Fioklou <kossifioklou10@gmail.com>
2023-06-30 16:59:06 -0700

    Fix linter errors

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,10 +1,9 @@
 # frozen_string_literal: true
 
 class OrderItem < ApplicationRecord
-    belongs_to :order
-    belongs_to :package
+  belongs_to :order
+  belongs_to :package
 
-    validates :quantity, presence: true
-    validates :quantity, numericality: { greater_than_or_equal_to: 1 }
-
+  validates :quantity, presence: true
+  validates :quantity, numericality: { greater_than_or_equal_to: 1 }
 end

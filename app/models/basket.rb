ab12a42da72bf997cb20b261d6cad6a14c32224b
Onomeh Onwuanibe <60798978+Estherstarr@users.noreply.github.com>
2023-07-15 14:06:40 +0000

    Fix linter error

--- app/models/basket.rb
+++ app/models/basket.rb
@@ -3,8 +3,7 @@
 class Basket < ApplicationRecord
   belongs_to :user
   has_many :basket_items, dependent: :destroy
-  has_many :products, :through :basket_items
-
+  has_many :products, through: :basket_items
 
   validates :user_id, uniqueness: true
 end

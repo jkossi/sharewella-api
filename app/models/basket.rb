74f823fd1633d9c8d35c9b746853fe9e81533079
Onomeh Onwuanibe <60798978+Estherstarr@users.noreply.github.com>
2023-07-15 14:02:45 +0000

    Update product and basket associations

--- app/models/basket.rb
+++ app/models/basket.rb
@@ -3,6 +3,8 @@
 class Basket < ApplicationRecord
   belongs_to :user
   has_many :basket_items, dependent: :destroy
+  has_many :products, :through :basket_items
+
 
   validates :user_id, uniqueness: true
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -15,6 +15,7 @@ class Product < ApplicationRecord
   has_many :order_items, dependent: :destroy
   has_many :orders, through: :order_items
   has_many :basket_items, dependent: :destroy
+  has_many :baskets, through: :basket_items
 
   monetize :price_cents, allow_nil: false, numericality: { greater_than_or_equal_to: 0 }
   monetize :retail_price_cents, allow_nil: false, numericality: { greater_than_or_equal_to: 0 }

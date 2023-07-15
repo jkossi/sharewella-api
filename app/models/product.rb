c105c6c46fac1a5fd71be22ce23436b7ad0315db
Onomeh Onwuanibe <60798978+Estherstarr@users.noreply.github.com>
2023-07-15 13:46:42 +0000

    Fix line error

--- app/models/product.rb
+++ app/models/product.rb
@@ -14,7 +14,6 @@ class Product < ApplicationRecord
   belongs_to :creator, class_name: "User"
   has_many :order_items, dependent: :destroy
   has_many :orders, through: :order_items
-
   has_many :basket_items, dependent: :destroy
 
   monetize :price_cents, allow_nil: false, numericality: { greater_than_or_equal_to: 0 }

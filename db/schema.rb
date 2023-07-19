9018c960612179c43a8520212342d2630fc626c0
Onomeh Onwuanibe <onwuanibeonome@gmail.com>
2023-07-19 12:38:26 +0000

    [SWB-33324632] Update Order_Item Model (#14)
    * Update order_item model
    
    * Update unique index name

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_11_115707) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_19_121645) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -52,6 +52,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_11_115707) do
     t.integer "quantity"
     t.datetime "updated_at", null: false
     t.index ["order_id"], name: "index_order_items_on_order_id"
+    t.index ["product_id", "order_id"], name: "index_order_items_on_product_id_and_order_id", unique: true
     t.index ["product_id"], name: "index_order_items_on_product_id"
   end
 

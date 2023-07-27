a0eace86a7676e128aac8fcd4453d987a2bcd0e5
Joseph Kossi A <42009253+jkossi@users.noreply.github.com>
2023-07-27 04:32:41 +0000

    chore: add top level exception handler

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
+  include ExceptionHandler
+
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
 
